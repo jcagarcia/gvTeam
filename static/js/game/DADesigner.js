9a82d2b60070ce97ef6a3efee8e0bcb1bf8204de
Juan Carlos García <jugade92@gmail.com>
2015-11-27 21:01:45 +0100

    Including sprite animations

--- static/js/game/DADesigner.js
+++ static/js/game/DADesigner.js
@@ -9,10 +9,26 @@ var DADesigner = (function() {
 		// Guardando cursores
 		this.cursors = cursors;
 
-		// Añadir sprite
-		this.designer = phaserGame.add.sprite(32, 32, 'char_designer');
+		// Generando diseñador
+		this.createDesigner();
 
 	    window.graphics = this.designer;
+	}
+
+
+	/**
+	* Método para crear el diseñador utilizando un sprite
+	*/
+	DADesigner.prototype.createDesigner = function(){
+		this.designer = this.game.add.sprite(32, 32, 'char_designer');
+		this.designer.scale.setTo(1.5,1.5);
+
+		// Añadimos animaciones
+		this.designer.animations.add("left", [0,1,2,3], 10, true);
+		this.designer.animations.add("right", [4,5,6,7], 10, true);
+		this.designer.animations.add("up", [8,9,10,11], 10, true);
+		this.designer.animations.add("down", [12,13,14,15], 10, true);
+
 	}
 
 	/**
@@ -22,24 +38,28 @@ var DADesigner = (function() {
 
 		// Moviendo hacia la derecha
 		if (this.cursors.right.isDown){
+
 			this.designer.x += velocidad;
-		}
+			this.designer.animations.play("right");
 
-		// Moviendo hacia la izquierda
-		if (this.cursors.left.isDown){
+		}else if (this.cursors.left.isDown){
+
 			this.designer.x -= velocidad;
-		}
+			this.designer.animations.play("left");
 
-		// Moviendo hacia arriba
-		if (this.cursors.up.isDown){
+		} else if (this.cursors.up.isDown){
+
 			this.designer.y -= velocidad;
-		}
+			this.designer.animations.play("up");
 
-		// Moviendo hacia arriba
-		if (this.cursors.down.isDown){
+		} else if (this.cursors.down.isDown){
+
 			this.designer.y += velocidad;
+			this.designer.animations.play("down");
+
+		} else {
+			this.designer.animations.stop();	
 		}
-    
 	}
 
 
