90ededb8dbd689859d44e06deb1f95be964cea59
Juan Carlos García <jugade92@gmail.com>
2015-11-27 18:21:02 +0100

    Including comments and initializing background color

--- static/js/game/DAGame.js
+++ static/js/game/DAGame.js
@@ -7,15 +7,31 @@ var DAGame = (function() {
 		this.game = phaserGame;
 	}
 
+	/**
+	* Método que inicializa el escenario básico del juego
+	*/
 	DAGame.prototype.init = function() {
+
+		// Setting background color
+		this.game.stage.backgroundColor = "#857868";
 	}
 
+	/**
+	* Método que carga los assets y recursos necesarios
+	*/
 	DAGame.prototype.preload = function() {
 	}
 
+	/**
+	* Método para crear el estado in¡cial del juego
+	*/
 	DAGame.prototype.create = function() {
 	}
 
+
+	/**
+	* Loop que actualizará el juego
+	*/
 	DAGame.prototype.update = function() {
 	}
 
